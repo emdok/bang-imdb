faf99cd4d2de055ff05a554311b0a5c57662117a
Emily Dokken <emily.dokken@instrument.com>
2021-12-21 19:39:14 -0800

    update search functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,42 +9,59 @@ var getIMDBMedia = function (title) {
     fetch(imdbQueryUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log("IMDB:", data);
-                getStreamAvailability(data.results[0].id);
+                var array = data.results;
+                console.log(array);
+                for (let i = 0; i < array.length; i++) {
+                    getStreamAvailability(array[i].id).catch(err => {
+                        console.log('error in getIMDBMedia is: ', err)
+                    })
+                }
                 getNytReviews(title);
             });
         } else {
             alert("Error: Title not found");
         }
+    }).catch(err => {
+        console.error('error in imdb media fetch: ', err);
     });
 };
 
 //Function to use IMDB ID to locate Streaming Services
-var getStreamAvailability = function (mediaId) {
-    fetch("https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=" + mediaId + "&output_language=en", {
+function getStreamAvailability (mediaId) {
+    try{
+    console.log('media Id is: ', mediaId) 
+    return fetch("https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=" + mediaId + "&output_language=en", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
             "x-rapidapi-key": "21f375a3cfmsh4f8395beb749419p1aaee6jsn187734a6f501"
+            // Retry-After: 10;
         }
-    })
-        .then(response => {
-            response.json().then(function (data) {
+    }).then(function (response){
+            if(response.status === 404){
+                alert('sorry this movie isn\'t availible');
+            } else {
+            return response.json().then(function (data) {
                 console.log("streamAvail:", data);
                 var banner = data.posterURLs.original;
                 var desc = data.overview;
                 var cast = data.cast;
                 var strmSrvc = data.streamingInfo;
-
                 console.log("banner:", banner);
                 console.log("Description:", desc);
                 console.log("Cast:", cast);
                 console.log("Streaming Services:", strmSrvc);
+            }).catch(err => {
+                console.error('error in .json: ', err)
             })
+            }
         })
         .catch(err => {
-            console.error(err);
+            console.error('error in fetchL ', err);
         });
+    } catch(err) {
+      console.log('error in catch block', err)
+    }
 }
 
 // API Call to NYTimes Reviews
@@ -77,3 +94,4 @@ searchQuery.addEventListener('keyup', function (event) {
     }
 });
 
+///
